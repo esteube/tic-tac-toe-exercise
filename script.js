c3ea99c9027fc329c0a661a9278e804164ec32f4
Elie Schoppik <eschoppik@gmail.com>
2016-01-19 21:08:34 -0800

    adding tests for tic tac toe

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = start;
 var boxes = document.getElementsByTagName("td");
 var turnText = document.querySelector(".playerTurn");
+var winnerText = document.querySelector(".winnerText");
 var counter = 1;
 var winCounter = 0;
 var OMoves = [];
 var XMoves = [];
+
+// document.getElementById("foosauce").addEventListener("click",function(){
+//   this.innerText = "dope";
+// });
 
 var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
 [0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
@@ -14,6 +19,12 @@ function start(){
   addResetListener();
 }
 
+function removeListeners(){
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    boxes[i].removeEventListener("click", addXorO);
+  }
+}
+
 function addXandOListener(){
   for (var i = boxes.length - 1; i >= 0; i--) {
     boxes[i].addEventListener("click", addXorO);
@@ -21,26 +32,26 @@ function addXandOListener(){
 }
 
 function addXorO(event){
-  if (event.target.innerHTML.length === 0){
+  if (event.target.innerText.length === 0){
     if (counter % 2 === 0) {
-      OMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "O";
+      OMoves.push(parseInt(event.target.getAttribute("id").replace(/[^0-9]/g,'')));
+      event.target.innerText = "O";
       event.target.setAttribute("class","O");
-      turnText.innerHTML = "It is X's turn";
+      turnText.innerText = "It is X's turn";
       counter++;
       checkForWin(OMoves, "O");
     }
     else {
-      XMoves.push(parseInt(event.target.getAttribute("data-num")));
-      event.target.innerHTML = "X";
+      XMoves.push(parseInt(event.target.getAttribute("id").replace(/[^0-9]/g,'')));
+      event.target.innerText = "X";
       event.target.setAttribute("class","X");
-      turnText.innerHTML = "It is O's turn";
+      turnText.innerText = "It is O's turn";
       counter++;
       checkForWin(XMoves, "X");
     }
   // if the counter is greater than or equal to 10, the game is a draw!
   if (counter >= 10){
-    turnText.innerHTML = "Game Over!";
+    turnText.innerText = "Game Over!";
     var conf = confirm("It's a draw, do you want to play again?");
     if(conf){
       resetBoard();
@@ -56,7 +67,7 @@ function addResetListener(){
 
 function checkForWin(movesArray, name){
   // loop over the first array of winning combinations
-  for (i = 0; i < winningCombinations.length; i++) {
+  for (var i = 0; i < winningCombinations.length; i++) {
     // reset the winCounter each time!
     winCounter = 0;
     // loop over each individual array
@@ -67,8 +78,8 @@ function checkForWin(movesArray, name){
       }
       // if winCounter === 3 that means all 3 moves are winning combos and game is over!
       if(winCounter === 3){
-        alert("Game over, " + name + " wins!");
-        resetBoard();
+        turnText.innerText = `Game over, ${name} wins!`;
+        removeListeners();
       }
     }
   }
@@ -76,12 +87,13 @@ function checkForWin(movesArray, name){
 
 function resetBoard(){
   for (var i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].innerHTML="";
+    boxes[i].innerText="";
     boxes[i].setAttribute("class","clear");
   }
   OMoves = [];
   XMoves = [];
   winCounter=0;
   counter = 1;
-  turnText.innerHTML = "It is X's turn";
+  turnText.innerText = "";
+  turnText.innerText = "It is X's turn";
 }
